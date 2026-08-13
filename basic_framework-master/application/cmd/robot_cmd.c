d2bed7f456c1df9bc28f23f8d68de501e166a35f
y-mouse <13466871656@163.com>
2026-08-13 19:43:51 +0800

    0.0.1,步兵代码初次创建

---
+++ basic_framework-master/application/cmd/robot_cmd.c
@@ -0,0 +1,39 @@
+// app
+#include "robot_def.h"
+#include "robot_cmd.h"
+// module
+#include "remote_control.h"
+#include "ins_task.h"
+#include "master_process.h"
+#include "message_center.h"
+#include "general_def.h"
+#include "dji_motor.h"
+#include "bmi088.h"
+// bsp
+#include "bsp_dwt.h"
+#include "bsp_log.h"
+
+// 私有宏,自动将编码器转换成角度值
+#define YAW_ALIGN_ANGLE (YAW_CHASSIS_ALIGN_ECD * ECD_ANGLE_COEF_DJI) // 对齐时的角度,0-360
+#define PTICH_HORIZON_ANGLE (PITCH_HORIZON_ECD * ECD_ANGLE_COEF_DJI) // pitch水平时电机的角度,0-360
+
+/* cmd应用包含的模块实例指针和交互信息存储*/
+
+
+
+
+BMI088Instance *bmi088_test; // 云台IMU
+BMI088_Data_t bmi088_data;
+void RobotCMDInit()
+{
+
+
+
+}
+
+
+/* 机器人核心控制任务,200Hz频率运行(必须高于视觉发送频率) */
+void RobotCMDTask()
+{
+
+}
